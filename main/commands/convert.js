cfa741537854d7365312b41f3485cbdce0a56398
Michael Shuff <mjshuff23@gmail.com>
2020-09-03 16:45:25 -0400

    removed decToBinary and added convert for bases 2-9

--- main/commands/convert.js
+++ main/commands/convert.js
@@ -1,25 +1,27 @@
 module.exports = {
-    name: 'dectobinary',
-    description: 'Converts decimal to binary',
+    name: 'convert',
+    description: 'Converts decimal to chose base',
     args: true,
-    usage: '[base 10 number]',
+    usage: '[base 10 number], base',
     guildOnly: false,
     cooldown: 5,
-    aliases: ['binary'],
+    aliases: ['base'],
     execute(message, args) {
         if (!args.length) return message.channel.send(`I need a number to convert.`);
         try {
             let number = Number(args[0]);
+            let base = Number(args[1]);
             let originalNum = number;
+            if (base > 10) return message.channel.send(`Sorry, I can't do bases higher than 10 yet :(`);
             if (isNaN(number) || number <= 0) return message.channel.send(`I need a number to convert.`);;
-            let binary = [];
+            let baseVal = [];
             while (number > 0) {
-                binary.push(number % 2);
-                number = Math.floor(number / 2);
+                baseVal.push(number % base);
+                number = Math.floor(number / base);
                 // console.log(number);
             }
 
-            return message.channel.send(`Number: ${originalNum} Binary: ${binary.reverse().join('')}`);
+            return message.channel.send(`Number: ${originalNum} base ${base}: ${baseVal.reverse().join('')}`);
         } catch (error) {
             console.error(error);
             message.channel.send(`There was an error somewhere. :)`);
